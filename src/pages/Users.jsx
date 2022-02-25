a1488bf96d0c524486a298dc438fe30af7c0a61f
LucasCandiago <lucas.candiago@gmail.com>
2022-02-25 13:40:08 -0300

    upload-01

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -6,13 +6,13 @@ function Users() {
             <h1>Usuários:</h1>
             {UsersList.map((UsersList, id) => (
                     <div className="user">
-                    <p><strong>{UsersList.Nome}</strong></p>                 
-                    <p>{UsersList.Cargo}</p>
-                    <p>{UsersList.Situacao}</p>
-                    <p>{UsersList.DataAdmissao}</p>  
-                    <p>{UsersList.DataIntegracao}</p>  
-                    <p>{UsersList.Time}</p>  
-                    <p>{UsersList.Cliente}</p>
+                    <h2>{UsersList.Nome}</h2>                 
+                    <p>Cargo: {UsersList.Cargo}</p>
+                    <p>Situação: {UsersList.Situacao}</p>
+                    <p>Data de admissão: {UsersList.DataAdmissao}</p>  
+                    <p>Data de integração: {UsersList.DataIntegracao}</p>  
+                    <p>Time: {UsersList.Time}</p>  
+                    <p>Cliente: {UsersList.Cliente}</p>
                     <br/>
                     </div>
                 ))}
